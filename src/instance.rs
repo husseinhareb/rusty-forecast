b7c2435dc108a31b75ce84f14521b24fd2add91b
husseinhareb <shtam.hareb12@gmail.com>
2024-04-04 05:46:47 +0300

    icons

--- src/instance.rs
+++ src/instance.rs
@@ -2,56 +2,24 @@ use serde::Deserialize;
 use serde_json::Value;
 use reqwest::blocking::Client;
 use crate::city::read_city_name;
+use crate::condition_icons::WeatherStatus;
 
 #[derive(Deserialize)]
 struct WeatherResponse {
     main: WeatherData,
     weather: Vec<WeatherDescription>,
-    wind: WindData,
-    visibility: Option<u32>,
-    clouds: Option<CloudData>,
-    sys: SysData,
+}
+
+#[derive(Deserialize)]
+struct WeatherData {
+    temp: f32,
+    humidity: f32,
 }
 
 #[derive(Deserialize)]
 struct WeatherDescription {
     description: String,
 }
-
-#[derive(Deserialize)]
-struct WindData {
-    speed: f32,
-    deg: f32,
-}
-
-#[derive(Deserialize)]
-struct CloudData {
-    all: f32,
-}
-
-#[derive(Deserialize)]
-struct SysData {
-    sunrise: u64,
-    sunset: u64,
-}
-
-
-#[derive(Deserialize)]
-struct WeatherData {
-    temp: f32,
-    humidity: f32,
-    description: String,
-    wind_speed: f32,
-    visibility: Option<f32>,
-    clouds: Option<f32>,
-    sunrise: Option<u64>,
-    sunset: Option<u64>,
-    pressure: Option<f32>,
-    temp_min: Option<f32>,
-    temp_max: Option<f32>,
-    feels_like: Option<f32>,
-}
-
 
 pub fn weather_now() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = "2a33d8b44aa8d93d07feac453b4a79aa";
@@ -75,7 +43,26 @@ pub fn weather_now() -> Result<(), Box<dyn std::error::Error>> {
 
     let weather: WeatherResponse = serde_json::from_value(data)?;
 
-    let temp_box = format!("╔═══════════════════════════╗\n║ Temperature: {:>6}°C                                   ║\n╚═══════════════════════════╝", weather.main.temp);
+    let weather_status = match weather.weather.get(0) {
+        Some(description) => match description.description.as_str() {
+            "clear sky" => WeatherStatus::clear_sky,
+            "few clouds" => WeatherStatus::few_clouds,
+            "scattered clouds" => WeatherStatus::scattered_clouds,
+            "broken clouds" => WeatherStatus::broken_clouds,
+            "shower rain" => WeatherStatus::shower_rain,
+            "rain" => WeatherStatus::rain,
+            "thunderstorm" => WeatherStatus::thunderstorm,
+            "snow" => WeatherStatus::snow,
+            "mist" => WeatherStatus::mist,
+            _ => WeatherStatus::mist, // Default to mist if unknown description
+        },
+        None => WeatherStatus::mist, // Default to mist if no weather description available
+    };
+
+    let temp_box = format!("╔═══════════════════════════╗\n║ Temperature: {:>6}°C\n║ Description: {:>12}    {}\n╚═══════════════════════════╝", 
+                           weather.main.temp, 
+                           weather.weather[0].description, 
+                           weather_status.icon());
 
     println!("{}", temp_box);
     
